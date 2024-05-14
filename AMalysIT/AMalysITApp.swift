63abc5641aeddfa4f0ef920f3631e92ad054696b
bgomiss <ditty-inning-0v@icloud.com>
2024-05-14 11:54:10 +0300

    Initial Commit

---
+++ AMalysIT/AMalysITApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMalysITApp.swift
+//  AMalysIT
+//
+//  Created by aycan duskun on 14.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AMalysITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
